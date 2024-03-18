91e37970acf82e313b8bb7340965db786ed94b11
Bhanuvidh Mansinghani <bhanuvidh.m@traveligo.com>
2024-03-18 17:35:00 +0700

    new cached

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="some-random-string")
@@ -33,36 +34,25 @@ def read_root():
 
 
 @app.get("/auth")
-def requesttoken(request: Request):
-    # Form the payload data
-    payload = {
-        "grant_type": "client_credentials",
-        "client_id": client_id,
-        "client_secret": client_secret,
-    }
-    # Set the headers
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    response = requests.post(
-        "https://accounts.spotify.com/api/token", data=payload, headers=headers
-    )
-    if response.status_code == 200:
-        response_data = response.json()
-        access_token = response_data.get("access_token")
-        request.session["access_token"] = access_token
-        return {"Access token:", access_token}
+def requesttoken(request: Request, code: str = "", state: str = ""):
 
+    if state != None:
+        redirectTo = "http://127.0.0.1:8000"
+        # Form the payload data
+        payload = {"grant_type": "authorization_code", "code": code, "redirect": redirectTo}
+        # Set the headers
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.post(
+            "https://accounts.spotify.com/api/token", data=payload, headers=headers
+        )
 
-def apigetcall(request: Request, url, bearer_token):
-
-    header = {"Authorization": "Bearer " + bearer_token}
-
-    response = requests.get(url, headers=header)
-
-    if response.status_code == 200:
-        response_data = response.json()
-        return response_data
+        if response.status_code == 200:
+            response_data = response.json()
+            access_token = response_data.get("access_token")
+            request.session["access_token"] = access_token
+            return {"Access token:", access_token}
     else:
-        return False
+        return RedirectResponse('/')
 
 
 @app.get("/artist")
@@ -85,7 +75,7 @@ def getartist(request: Request):
 
 @app.get("/me")
 def getuserdata(request: Request):
-    
+
     bearer_token = request.session.get("access_token")
 
     url = "https://api.spotify.com/v1/me"
@@ -96,8 +86,26 @@ def getuserdata(request: Request):
 
     if response.status_code == 200:
         return {"status": True, "data": str(response)}
-    elif response.status_code == 401 :
-        response_data = response.json()
-        return {"status": response_data['error']['message']}
     else:
         return {"status": False}
+
+
+@app.get("/login")
+def login_spotify():
+    scope = "user-read-private user-read-email"
+    redirect_uri = "http://127.0.0.1:8000"
+    state = "ahkjasfdkfureertfknf"
+
+    return RedirectResponse(
+        "https://accounts.spotify.com/authorize?"
+        + "response_type="
+        + "code"
+        + "&client_id="
+        + client_id
+        + "&scope="
+        + scope
+        + "&redirect_uri="
+        + redirect_uri
+        + "&state="
+        + state
+    )
