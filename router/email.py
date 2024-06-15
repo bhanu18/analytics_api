0eb45722ec9fcc2731578104e3df201b0b128356
Bhanuvidh Mansinghani <bhanuvidh@windowslive.com>
2024-06-15 07:20:30 +0000

    change_names

--- router/email.py
+++ router/email.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.post('/', tags=["users"]
- async def read_users():
+@router.post('/', tags=["email"]
+ async def send_mail():
    return [{"username": "Rick"}, {"username": "Morty"}])
